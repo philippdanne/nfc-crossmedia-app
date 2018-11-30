76845495ec1269496d755be3a23cda09e97257a6
Philipp Danne <philipp@bandship.com>
2018-11-30 10:35:37 +0100

    NFC-Tag wird erkannt und dessen Inhalt in der App angezeigt

--- app/src/main/java/de/dhbw/mosbach/nfccrossmedia/NfcTagDiscovered.java
+++ app/src/main/java/de/dhbw/mosbach/nfccrossmedia/NfcTagDiscovered.java
@@ -1,7 +1,13 @@
 package de.dhbw.mosbach.nfccrossmedia;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class NfcTagDiscovered extends AppCompatActivity {
 
@@ -9,5 +15,42 @@ public class NfcTagDiscovered extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfc_tag_discovered);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage message = (NdefMessage) rawMessages[0];
+                NdefRecord dataRecord = message.getRecords()[1];
+
+                String payloadString = new String(dataRecord.getPayload());
+
+                TextView nfcTextView = findViewById(R.id.nfcTextView);
+                nfcTextView.setText(payloadString);
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage message = (NdefMessage) rawMessages[0];
+                NdefRecord dataRecord = message.getRecords()[1];
+
+                String payloadString = new String(dataRecord.getPayload());
+
+                TextView nfcTextView = findViewById(R.id.nfcTextView);
+                nfcTextView.setText(payloadString);
+            }
+        }
     }
 }
